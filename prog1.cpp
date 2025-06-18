9a39835b6ac1397f09043c577525ed2af3d909d4
unknown <shaikhnurul8200@gmail.com>
2025-06-18 23:50:31 +0530

    pr

--- prog1.cpp
+++ prog1.cpp
@@ -2,19 +2,14 @@
 #include <vector>
 using namespace std;
 
-class SortingAlgorithms
-{
+class SortingAlgorithms {
 public:
-    int selectionSort(vector<int> &arr, int n)
-    {
+    void selectionSort(vector<int> &arr, int n) {
         int min, i, j;
-        for (i = 0; i < n - 1; i++)
-        {
+        for (i = 0; i < n - 1; i++) {
             min = i;
-            for (j = i + 1; j < n; j++)
-            {
-                if (arr[j] < arr[min])
-                {
+            for (j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
                     min = j;
                 }
             }
@@ -22,49 +17,39 @@ public:
         }
     }
 
-public:
-    int mergeSorting(vector<int> &arr, int start, int mid, int end)
-    {
+    void mergeSorting(vector<int> &arr, int start, int mid, int end) {
         vector<int> temp;
         int i = start, j = mid + 1;
 
-        while (i <= mid && j <= end)
-        {
-            if (arr[i] < arr[j])
-            {
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
                 temp.push_back(arr[i++]);
-            }
-            else
-            {
+            } else {
                 temp.push_back(arr[j++]);
             }
         }
-        while (i <= mid)
-        {
+        while (i <= mid) {
             temp.push_back(arr[i++]);
         }
-        while (j <= end)
-        {
+        while (j <= end) {
             temp.push_back(arr[j++]);
         }
-        for (int k = 0; k < temp.size(); k++)
-        {
+        for (int k = 0; k < temp.size(); k++) {
             arr[start + k] = temp[k];
         }
     }
 
-    int mergeSort(vector<int> &arr, int start, int end)
-    {
+    void mergeSort(vector<int> &arr, int start, int end) {
         if (start >= end)
             return;
         int mid = (start + end) / 2;
 
-        mergeSort(arr, start, mid - 1);
+        mergeSort(arr, start, mid);
         mergeSort(arr, mid + 1, end);
         mergeSorting(arr, start, mid, end);
     }
 
-    bool sortcheck(vector<int> &arr, int n){
+    bool sortcheck(vector<int> &arr, int n) {
         for (int i = 1; i < n; i++) {
             if (arr[i] < arr[i - 1]) {
                 return false;
@@ -74,41 +59,31 @@ public:
     }
 };
 
-class SearchingAlgorithms
-{
+class SearchingAlgorithms {
 public:
-    int linearSearch(vector<int> &arr, int element)
-    {
-        for (int i = 0; i < arr.size(); i++)
-        {
-            if (arr[i] == element)
-            {
+    int linearSearch(vector<int> &arr, int element) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == element) {
                 return i;
             }
         }
         return -1;
     }
 
-    int binarySearch(vector<int> &arr, int element)
-    {
+    int binarySearch(vector<int> &arr, int element) {
         int left = 0, right = arr.size() - 1, found = -1;
-        while (left <= right)
-        {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == element)
-            {
+            if (arr[mid] == element) {
                 found = mid;
                 break;
-            }
-            else if (arr[mid] < element)
-            {
+            } else if (arr[mid] < element) {
                 left = mid + 1;
-            }
-            else
-            {
+            } else {
                 right = mid - 1;
             }
         }
+
         if (found != -1)
             cout << "Element found at index: " << found << endl;
         else
@@ -117,52 +92,43 @@ public:
     }
 };
 
-class Show : public SortingAlgorithms, public SearchingAlgorithms
-{
+class Show : public SortingAlgorithms, public SearchingAlgorithms {
 public:
-    int display(vector<int> &arr, int n)
-    {
-        for (int i = 0; i < arr.size(); i++)
-        {
+    void display(vector<int> &arr, int n) {
+        for (int i = 0; i < arr.size(); i++) {
             cout << arr[i] << " ";
         }
+        cout << endl;
     }
 };
 
-int main()
-{
+int main() {
     Show sort;
     int n, choice;
-    cout << "Enter the size of array : ";
+    cout << "Enter the size of array: ";
     cin >> n;
 
     vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter " << n << "element of array : ";
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
         cin >> arr[i];
     }
 
-    int result1 = sort.selectionSort(arr, n);
-    int result2 = sort.mergeSort(arr, 0, n - 1);
-
-    do
-    {
+    do {
         cout << "\n========================================\n";
-        cout << "0. Exit  Program \n";
-        cout << "1. Display Array \n";
-        cout << "2. Selection Sort \n";
-        cout << "3. Merge Sort \n";
-        cout << "4. Linear Search \n";
+        cout << "0. Exit Program\n";
+        cout << "1. Display Array\n";
+        cout << "2. Selection Sort\n";
+        cout << "3. Merge Sort\n";
+        cout << "4. Linear Search\n";
         cout << "5. Binary Search (APPLICABLE AFTER SORTING)\n";
-        cout<<"Enter your choice : ";
-        cin>>choice;
-        cout<<endl;
-        switch (choice)
-        {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cout << endl;
 
+        switch (choice) {
         case 0:
-            cout << "\n Successfully Exited The Program!!!\n";
+            cout << "\nSuccessfully Exited The Program!!!\n";
             break;
 
         case 1:
@@ -171,14 +137,15 @@ int main()
 
         case 2:
             sort.selectionSort(arr, n);
+            cout << "Array sorted using Selection Sort.\n";
             break;
 
         case 3:
             sort.mergeSort(arr, 0, n - 1);
+            cout << "Array sorted using Merge Sort.\n";
             break;
 
-        case 4:
-
+        case 4: {
             int element;
             cout << "Enter element to search: ";
             cin >> element;
@@ -188,21 +155,24 @@ int main()
             else
                 cout << "Element not found." << endl;
             break;
+        }
 
-        case 5:
-        {
-            if(sort.sortcheck(arr , n) == true){
-            int element;
-            cout << "Enter element to search: ";
-            cin >> element;
-            sort.binarySearch(arr, element);
-            }
-            else{
-                cout<<"Array should be sorted ";
+        case 5: {
+            if (sort.sortcheck(arr, n)) {
+                int element;
+                cout << "Enter element to search: ";
+                cin >> element;
+                sort.binarySearch(arr, element);
+            } else {
+                cout << "Array is not sorted. Please sort it first using option 2 or 3.\n";
             }
             break;
         }
+
+        default:
+            cout << "Invalid choice. Please try again.\n";
         }
+
     } while (choice != 0);
 
     return 0;
